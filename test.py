d13171f6d27b4e603f9a4d10fe80d6fd19df0850
me <pavel@pavelkral.eu>
2018-03-11 16:19:26 +0100

    Test tool

---
+++ test.py
@@ -0,0 +1,15 @@
+
+from ethereum_caller.call_smart_contract import CallSmartContract
+
+# This tool is used for manual calling of the smart contract
+
+caller = CallSmartContract()
+
+
+# Card 1
+# caller.register_new_card("04A6DE1A594980")
+
+# Card 2
+# caller.register_new_card("049FB11A594980")
+
+# caller.iterate_birds(45)
