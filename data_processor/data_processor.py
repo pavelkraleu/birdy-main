8bcff8d4e3e3c850bf67b9c0789274126f2e831f
me <pavel@pavelkral.eu>
2018-03-11 16:22:14 +0100

    Call SC from Data Processor

--- data_processor/data_processor.py
+++ data_processor/data_processor.py
@@ -2,6 +2,7 @@ import datetime
 from weather import Weather
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
+from ethereum_caller.call_smart_contract import CallSmartContract
 
 
 class DataProcessor(object):
@@ -22,6 +23,8 @@ class DataProcessor(object):
         self.birds_sheet = gc.open(self.google_sheet_id).worksheet("Sheet1")
         self.cards_sheet = gc.open(self.google_sheet_id).worksheet("Sheet2")
 
+        self.smart_contract_caller = CallSmartContract()
+
     def submit_data(self, number_of_birds):
 
         location = self.weather.lookup_by_location('Prague')
@@ -33,6 +36,11 @@ class DataProcessor(object):
 
         self.birds_sheet.append_row([datetime.datetime.now(), number_of_birds, condition_text, temperature])
 
+        self.smart_contract_caller.iterate_birds(number_of_birds)
+
     def submit_card(self, card_id):
 
         self.cards_sheet.append_row([datetime.datetime.now(), card_id])
+
+        self.smart_contract_caller.register_new_card(card_id)
+
